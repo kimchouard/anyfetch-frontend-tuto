9742beeb8b3cccb606ba341c1cdc0a2d137ca269
Kim CHOUARD <kim.chouard@gmail.com>
2014-06-05 15:24:25 +0200

    Authentification & refactoring

--- scripts/anyfetch.js
+++ scripts/anyfetch.js
@@ -1,14 +1,35 @@
-// Get the API status
-var jqxhr = $.ajax({
-		type: 'GET',
-		dataType: 'json',
-		url: 'https://api.anyfetch.com/status'
-	})
-	// All went well
-  .done(function(data) {
-    $('#apiStatus').html(data.status);
-  })
-  // An error occured
-  .fail(function(jqXHR, textStatus) {
-    console.log("An error occured: ", textStatus, jqXHR );
-  });
+// base64 encoding for the login
+var login64 = btoa('test@anyfetch.com:password');
+
+// Do an authentificated API call
+var apiCall = function(endpoint, callback) {
+	$.ajax({
+			type: 'GET',
+			dataType: 'json',
+			url: 'https://api.anyfetch.com'+endpoint,
+			headers: {
+				'Authorization': 'Basic '+login64
+			}
+		})
+		// All went well
+	  .done(function(data) {
+	    callback(data, null);
+	  })
+	  // An error occured
+	  .fail(function(jqXHR, textStatus) {
+	    console.log("An error occured: ", textStatus, jqXHR );
+	    callback(null, textStatus);
+	  });
+}
+
+$(function() {
+	// Get API status
+	apiCall('/status', function(data, err) {
+		if (err) {
+			return err;
+		}
+
+		// Update API status on index.html
+		$('#apiStatus').html(data.status);
+	});
+});
