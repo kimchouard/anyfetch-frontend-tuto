4cdbfc1860b5981b189edee4c7d4081bcaf25ca5
Kim CHOUARD <kim.chouard@gmail.com>
2014-06-05 15:10:57 +0200

    Bind API status on html

--- scripts/anyfetch.js
+++ scripts/anyfetch.js
@@ -1,11 +1,14 @@
+// Get the API status
 var jqxhr = $.ajax({
 		type: 'GET',
 		dataType: 'json',
 		url: 'https://api.anyfetch.com/status'
 	})
+	// All went well
   .done(function(data) {
-    console.log( data );
+    $('#apiStatus').html(data.status);
   })
+  // An error occured
   .fail(function(jqXHR, textStatus) {
-    console.log(textStatus, jqXHR );
+    console.log("An error occured: ", textStatus, jqXHR );
   });
